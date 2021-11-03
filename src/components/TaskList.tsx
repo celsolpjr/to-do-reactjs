a7da5de169638891d3de2862461d213359186b94
Celso <celso.8882011@gmail.com>
2021-11-03 20:48:59 -0300

    finalize project

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,41 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
+    if (newTaskTitle == '') {
+      window.alert("Campo em branco!");
+      return
+    } 
+    else {
+      setTasks([ ...tasks, {
+        id: Math.floor(Math.random() * 100),
+        title: newTaskTitle,
+        isComplete: false
+      }])
+
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    const newTasks = tasks.map((task) => {
+      if (task.id == id) {
+        task.isComplete == false ? task.isComplete = true : task.isComplete = false;
+      }
+      return task
+    })
+
+    setTasks(newTasks);
+
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    
+    const newTasks = tasks.filter(task => task.id != id);
+
+    setTasks(newTasks);
+
   }
 
   return (
